327fbd7a228fd8813f18169298064aca33b29581
asahi <miuraasahi1@gmail.com>
2021-07-19 18:27:22 +0900

    added all files

---
+++ account/views.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import AuthenticationForm
+
+# Create your views here.
+
+
+class LoginView(LoginView):
+
+    template_name = 'account/login.html'
+    form_class = AuthenticationForm
+
+login = LoginView.as_view()
